f01adffe759585c968b0161da498b5290c8784de
JuanRosas <juanmrosas8@gmail.com>
2024-05-24 19:53:03 -0300

    Creacion Activity para botones

---
+++ app/src/main/java/com/juanrosasdev/toolandroid/BotonesActivity.kt
@@ -0,0 +1,12 @@
+package com.juanrosasdev.toolandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BotonesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_botones)
+
+    }
+}
