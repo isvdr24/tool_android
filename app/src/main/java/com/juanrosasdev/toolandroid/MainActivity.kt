564ad0c9d29dcfb6f8723acdb7a65e093ddd170a
AntonellaRe <ma.antonellare@gmail.com>
2024-06-07 19:10:18 -0300

    Resueltos los conflictos de merge en MainActivity.kt combinando los cambios

--- app/src/main/java/com/juanrosasdev/toolandroid/MainActivity.kt
+++ app/src/main/java/com/juanrosasdev/toolandroid/MainActivity.kt
@@ -6,20 +6,13 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-
-
-=======
         funciondeprueba()
     }
+
     private fun funciondeprueba(){
         Toast.makeText(this, "Esto es una prueba", Toast.LENGTH_SHORT).show()
->>>>>>> 2803ba91362038f0ecbb5f16cc422c77b172df73
     }
-
-
 }
